e289208f9972c8bce3afff837e870b80d8832259
GdoongMathew <gdoongmathew@gmail.com>
2024-03-31 21:39:35 +0800

    refactor: refactor `MultiplicativeLR` scheduler.

--- torch/optim/lr_scheduler.py
+++ torch/optim/lr_scheduler.py
@@ -349,7 +349,7 @@ class LambdaLR(Scheduler):
         return [base_lr["lr"] * lmbda(last_step) for lmbda, base_lr in zip(self.lr_lambdas, self.base_targets)]
 
 
-class MultiplicativeLR(LRScheduler):
+class MultiplicativeLR(LambdaLR):
     """Multiply the learning rate of each parameter group by the factor given
     in the specified function. When last_epoch=-1, sets initial lr as lr.
 
@@ -359,7 +359,6 @@ class MultiplicativeLR(LRScheduler):
             factor given an integer parameter epoch, or a list of such
             functions, one for each group in optimizer.param_groups.
         last_epoch (int): The index of last epoch. Default: -1.
-        verbose (bool): If ``True``, prints a message to stdout for
             each update. Default: ``False``.
 
             .. deprecated:: 2.2
@@ -376,61 +375,15 @@ class MultiplicativeLR(LRScheduler):
         >>>     scheduler.step()
     """
 
-    def __init__(self, optimizer, lr_lambda, last_epoch=-1, verbose="deprecated"):
-        self.optimizer = optimizer
+    @property
+    def targets(self) -> Sequence[str]:
+        return ['lr']
 
-        if not isinstance(lr_lambda, list) and not isinstance(lr_lambda, tuple):
-            self.lr_lambdas = [lr_lambda] * len(optimizer.param_groups)
-        else:
-            if len(lr_lambda) != len(optimizer.param_groups):
-                raise ValueError(f"Expected {len(optimizer.param_groups)} lr_lambdas, but got {len(lr_lambda)}")
-            self.lr_lambdas = list(lr_lambda)
-        super().__init__(optimizer, last_epoch, verbose)
-
-    def state_dict(self):
-        """Returns the state of the scheduler as a :class:`dict`.
-
-        It contains an entry for every variable in self.__dict__ which
-        is not the optimizer.
-        The learning rate lambda functions will only be saved if they are callable objects
-        and not if they are functions or lambdas.
-        """
-        state_dict = {key: value for key, value in self.__dict__.items() if key not in ('optimizer', 'lr_lambdas')}
-        state_dict['lr_lambdas'] = [None] * len(self.lr_lambdas)
-
-        for idx, fn in enumerate(self.lr_lambdas):
-            if not isinstance(fn, types.FunctionType):
-                state_dict['lr_lambdas'][idx] = fn.__dict__.copy()
-
-        return state_dict
-
-    def load_state_dict(self, state_dict):
-        """Loads the schedulers state.
-
-        Args:
-            state_dict (dict): scheduler state. Should be an object returned
-                from a call to :meth:`state_dict`.
-        """
-        lr_lambdas = state_dict.pop('lr_lambdas')
-        self.__dict__.update(state_dict)
-        # Restore state_dict keys in order to prevent side effects
-        # https://github.com/pytorch/pytorch/issues/32756
-        state_dict['lr_lambdas'] = lr_lambdas
-
-        for idx, fn in enumerate(lr_lambdas):
-            if fn is not None:
-                self.lr_lambdas[idx].__dict__.update(fn)
-
-    def get_lr(self):
+    def get_targets(self, *, step, **kwargs):
         if not self._get_lr_called_within_step:
             warnings.warn("To get the last learning rate computed by the scheduler, "
-                          "please use `get_last_lr()`.", UserWarning)
-
-        if self.last_epoch > 0:
-            return [group['lr'] * lmbda(self.last_epoch)
-                    for lmbda, group in zip(self.lr_lambdas, self.optimizer.param_groups)]
-        else:
-            return [group['lr'] for group in self.optimizer.param_groups]
+                          "please use `get_last_lr()`.")
+        return [targets["lr"] * lmbda(step) for lmbda, targets in zip(self.lr_lambdas, self.last_targets)]
 
 
 class StepLR(LRScheduler):
