4ca4547823189d7ed9952eaf92488474b943db76
GdoongMathew <gdoongmathew@gmail.com>
2024-04-06 01:05:51 +0800

    refactor: refactor `CosineAnnealingWarmRestarts`.

--- torch/optim/lr_scheduler.py
+++ torch/optim/lr_scheduler.py
@@ -1528,7 +1528,7 @@ class CyclicLR(Scheduler):
         self._init_scale_fn()
 
 
-class CosineAnnealingWarmRestarts(LRScheduler):
+class CosineAnnealingWarmRestarts(Scheduler):
     r"""Set the learning rate of each parameter group using a cosine annealing
     schedule, where :math:`\eta_{max}` is set to the initial lr, :math:`T_{cur}`
     is the number of epochs since the last restart and :math:`T_{i}` is the number
@@ -1561,7 +1561,15 @@ class CosineAnnealingWarmRestarts(LRScheduler):
         https://arxiv.org/abs/1608.03983
     """
 
-    def __init__(self, optimizer, T_0, T_mult=1, eta_min=0, last_epoch=-1, verbose="deprecated"):
+    def __init__(
+            self,
+            optimizer: Optimizer,
+            T_0: int,
+            T_mult: int = 1,
+            eta_min: Union[int, float] = 0,
+            last_step: int = -1,
+            **kwargs,
+    ):
         if T_0 <= 0 or not isinstance(T_0, int):
             raise ValueError(f"Expected positive integer T_0, but got {T_0}")
         if T_mult < 1 or not isinstance(T_mult, int):
@@ -1572,18 +1580,21 @@ class CosineAnnealingWarmRestarts(LRScheduler):
         self.T_i = T_0
         self.T_mult = T_mult
         self.eta_min = eta_min
-        self.T_cur = last_epoch
-        super().__init__(optimizer, last_epoch, verbose)
+        self.T_cur = last_step
+        super().__init__(optimizer, last_step=last_step, **kwargs)
+
+    @property
+    def targets(self) -> Sequence[str]:
+        return ["lr"]
 
     def get_lr(self):
-        if not self._get_lr_called_within_step:
-            warnings.warn("To get the last learning rate computed by the scheduler, "
-                          "please use `get_last_lr()`.", UserWarning)
+        return [
+            {self.targets[0]: self.eta_min + (base_target[self.targets[0]] - self.eta_min) * (
+                        1 + math.cos(math.pi * self.T_cur / self.T_i)) / 2}
+            for base_target in self.base_targets
+        ]
 
-        return [self.eta_min + (base_lr - self.eta_min) * (1 + math.cos(math.pi * self.T_cur / self.T_i)) / 2
-                for base_lr in self.base_lrs]
-
-    def step(self, epoch=None):
+    def get_targets(self, *, step: int, **kwargs) -> Optional[Sequence[Dict[str, Any]]]:
         """Step could be called after every batch update
 
         Example:
@@ -1610,50 +1621,21 @@ class CosineAnnealingWarmRestarts(LRScheduler):
             >>> scheduler.step(26)
             >>> scheduler.step() # scheduler.step(27), instead of scheduler(20)
         """
-
-        if epoch is None and self.last_epoch < 0:
-            epoch = 0
-
-        if epoch is None:
-            epoch = self.last_epoch + 1
-            self.T_cur = self.T_cur + 1
-            if self.T_cur >= self.T_i:
-                self.T_cur = self.T_cur - self.T_i
-                self.T_i = self.T_i * self.T_mult
-        else:
-            if epoch < 0:
-                raise ValueError(f"Expected non-negative epoch, but got {epoch}")
-            if epoch >= self.T_0:
-                if self.T_mult == 1:
-                    self.T_cur = epoch % self.T_0
-                else:
-                    n = int(math.log((epoch / self.T_0 * (self.T_mult - 1) + 1), self.T_mult))
-                    self.T_cur = epoch - self.T_0 * (self.T_mult ** n - 1) / (self.T_mult - 1)
-                    self.T_i = self.T_0 * self.T_mult ** (n)
+        if not self._get_lr_called_within_step:
+            warnings.warn("To get the last learning rate computed by the scheduler, "
+                          "please use `get_last_lr()`.", UserWarning)
+        if step >= self.T_0:
+            if self.T_mult == 1:
+                self.T_cur = step % self.T_0
             else:
-                self.T_i = self.T_0
-                self.T_cur = epoch
-        self.last_epoch = math.floor(epoch)
+                n = int(math.log((step / self.T_0 * (self.T_mult - 1) + 1), self.T_mult))
+                self.T_cur = step - self.T_0 * (self.T_mult ** n - 1) / (self.T_mult - 1)
+                self.T_i = self.T_0 * self.T_mult ** n
+        else:
+            self.T_i = self.T_0
+            self.T_cur = step
 
-        class _enable_get_lr_call:
-
-            def __init__(self, o):
-                self.o = o
-
-            def __enter__(self):
-                self.o._get_lr_called_within_step = True
-                return self
-
-            def __exit__(self, type, value, traceback):
-                self.o._get_lr_called_within_step = False
-                return self
-
-        with _enable_get_lr_call(self):
-            for i, data in enumerate(zip(self.optimizer.param_groups, self.get_lr())):
-                param_group, lr = data
-                param_group['lr'] = lr
-
-        self._last_lr = [group['lr'] for group in self.optimizer.param_groups]
+        return self.get_lr()
 
 
 class OneCycleLR(LRScheduler):
