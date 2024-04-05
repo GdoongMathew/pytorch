00c565967c66c7ef266eac1f8ebe6531f8b65855
GdoongMathew <gdoongmathew@gmail.com>
2024-04-05 22:55:36 +0800

    refactor: refactor `ReduceLROnPlateau`.

--- torch/optim/lr_scheduler.py
+++ torch/optim/lr_scheduler.py
@@ -1051,7 +1051,7 @@ class ChainedScheduler(LRScheduler):
             self._schedulers[idx].load_state_dict(s)
 
 
-class ReduceLROnPlateau(LRScheduler):
+class ReduceLROnPlateau(Scheduler):
     """Reduce learning rate when a metric has stopped improving.
     Models often benefit from reducing the learning rate by a factor
     of 2-10 once learning stagnates. This scheduler reads a metrics
@@ -1111,82 +1111,103 @@ class ReduceLROnPlateau(LRScheduler):
         >>>     scheduler.step(val_loss)
     """
 
-    def __init__(self, optimizer, mode='min', factor=0.1, patience=10,
-                 threshold=1e-4, threshold_mode='rel', cooldown=0,
-                 min_lr=0, eps=1e-8, verbose="deprecated"):
+    def __init__(
+            self,
+            optimizer: Optimizer,
+            param_groups: Optional[Sequence[Dict[str, Any]]] = None,
+            mode: Literal["min", "max"] = 'min',
+            factor: float = 0.1,
+            patience: int = 10,
+            threshold: float = 1e-4,
+            threshold_mode: Literal["rel", "abs"] = "rel",
+            cooldown: int = 0,
+            min_lr: Union[float, Sequence[float]] = 0.0,
+            eps: float = 1e-8,
+            **kwargs,
+    ):
 
-        if factor >= 1.0:
-            raise ValueError('Factor should be < 1.0.')
+        if factor >= 1.0 or factor < 0.0:
+            raise ValueError('Factor should be 0.0 <= x < 1.0.')
         self.factor = factor
 
-        # Attach optimizer
-        if not isinstance(optimizer, Optimizer):
-            raise TypeError(f'{type(optimizer).__name__} is not an Optimizer')
-        self.optimizer = optimizer
+        param_groups = param_groups or optimizer.param_groups
 
+        # Attach optimizer
         if isinstance(min_lr, (list, tuple)):
-            if len(min_lr) != len(optimizer.param_groups):
-                raise ValueError(f"expected {len(optimizer.param_groups)} min_lrs, got {len(min_lr)}")
+            if len(min_lr) != len(param_groups):
+                raise ValueError(f"expected {len(param_groups)} min_lrs, got {len(min_lr)}")
             self.min_lrs = list(min_lr)
         else:
-            self.min_lrs = [min_lr] * len(optimizer.param_groups)
+            self.min_lrs = [min_lr] * len(param_groups)
 
         self.patience = patience
 
-        self.verbose = _check_verbose_deprecated_warning(verbose)
         self.cooldown = cooldown
         self.cooldown_counter = 0
+        self.num_bad_iters = 0
+
         self.mode = mode
         self.threshold = threshold
         self.threshold_mode = threshold_mode
+
         self.best = None
-        self.num_bad_epochs = None
         self.mode_worse = None  # the worse value for the chosen mode
         self.eps = eps
         self.last_epoch = 0
-        self._last_lr = [group['lr'] for group in self.optimizer.param_groups]
-        self._init_is_better(mode=mode, threshold=threshold,
-                             threshold_mode=threshold_mode)
+
+        self._init_is_better()
         self._reset()
+
+        super().__init__(optimizer=optimizer, param_groups=param_groups, **kwargs)
+
+    @property
+    def targets(self) -> Sequence[str]:
+        return ["lr"]
 
     def _reset(self):
         """Resets num_bad_epochs counter and cooldown counter."""
         self.best = self.mode_worse
         self.cooldown_counter = 0
-        self.num_bad_epochs = 0
+        self.num_bad_iters = 0
 
-    def step(self, metrics, epoch=None):
-        # convert `metrics` to float, in case it's a zero-dim Tensor
+    def get_targets(
+            self,
+            *,
+            step: int,
+            metrics: float,
+            **kwargs
+    ) -> Optional[Sequence[Dict[str, Any]]]:
+
+        if not step:
+            return
+
         current = float(metrics)
-        if epoch is None:
-            epoch = self.last_epoch + 1
-        else:
-            warnings.warn(EPOCH_DEPRECATION_WARNING, UserWarning)
-        self.last_epoch = epoch
-
         if self.is_better(current, self.best):
             self.best = current
-            self.num_bad_epochs = 0
+            self.num_bad_iters = 0
         else:
-            self.num_bad_epochs += 1
+            self.num_bad_iters += 1
 
         if self.in_cooldown:
             self.cooldown_counter -= 1
-            self.num_bad_epochs = 0  # ignore any bad epochs in cooldown
+            self.num_bad_iters = 0  # ignore any bad epochs in cooldown
 
-        if self.num_bad_epochs > self.patience:
-            self._reduce_lr(epoch)
+        if self.num_bad_iters > self.patience:
+            lrs = self._reduce_lr()
             self.cooldown_counter = self.cooldown
-            self.num_bad_epochs = 0
+            self.num_bad_iters = 0
+        else:
+            lrs = None
 
-        self._last_lr = [group['lr'] for group in self.optimizer.param_groups]
+        return lrs
 
-    def _reduce_lr(self, epoch):
-        for i, param_group in enumerate(self.optimizer.param_groups):
+    def _reduce_lr(self) -> Sequence[Dict[str, Any]]:
+        lrs = [{} for _ in range(len(self.param_groups))]
+        for i, (param_group, min_lr) in enumerate(zip(self.param_groups, self.min_lrs)):
             old_lr = float(param_group['lr'])
-            new_lr = max(old_lr * self.factor, self.min_lrs[i])
-            if old_lr - new_lr > self.eps:
-                param_group['lr'] = new_lr
+            new_lr = max(old_lr * self.factor, min_lr)
+            lrs[i] = {"lr": new_lr if old_lr - new_lr > self.eps else old_lr}
+        return lrs
 
     @property
     def in_cooldown(self):
@@ -1197,37 +1218,27 @@ class ReduceLROnPlateau(LRScheduler):
             rel_epsilon = 1. - self.threshold
             return a < best * rel_epsilon
 
-        elif self.mode == 'min' and self.threshold_mode == 'abs':
+        if self.mode == 'min' and self.threshold_mode == 'abs':
             return a < best - self.threshold
 
-        elif self.mode == 'max' and self.threshold_mode == 'rel':
+        if self.mode == 'max' and self.threshold_mode == 'rel':
             rel_epsilon = self.threshold + 1.
             return a > best * rel_epsilon
 
-        else:  # mode == 'max' and epsilon_mode == 'abs':
-            return a > best + self.threshold
+        # mode == 'max' and epsilon_mode == 'abs':
+        return a > best + self.threshold
 
-    def _init_is_better(self, mode, threshold, threshold_mode):
-        if mode not in {'min', 'max'}:
-            raise ValueError('mode ' + mode + ' is unknown!')
-        if threshold_mode not in {'rel', 'abs'}:
-            raise ValueError('threshold mode ' + threshold_mode + ' is unknown!')
+    def _init_is_better(self):
+        if self.mode not in {"min", "max"}:
+            raise ValueError(f"mode `{self.mode}` is unknown!")
+        if self.threshold_mode not in {'rel', 'abs'}:
+            raise ValueError(f"threshold mode {self.threshold_mode} is unknown!")
 
-        if mode == 'min':
-            self.mode_worse = inf
-        else:  # mode == 'max':
-            self.mode_worse = -inf
-
-        self.mode = mode
-        self.threshold = threshold
-        self.threshold_mode = threshold_mode
-
-    def state_dict(self):
-        return {key: value for key, value in self.__dict__.items() if key != 'optimizer'}
+        self.mode_worse = inf if self.mode == 'min' else -inf
 
     def load_state_dict(self, state_dict):
-        self.__dict__.update(state_dict)
-        self._init_is_better(mode=self.mode, threshold=self.threshold, threshold_mode=self.threshold_mode)
+        super().load_state_dict(state_dict)
+        self._init_is_better()
 
 
 class CyclicLR(Scheduler):
